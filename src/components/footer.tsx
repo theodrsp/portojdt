821d36a898bd1a0ee360a25c11b1c3f66083ad74
TIMOTIUS THEODEARSON <theodrsp@gmail.com>
2024-07-25 01:21:39 +0700

    [update} clean/remove unusually code 2

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-
 const Footer = () => {
   return (
-    <div className='flex flex-col w-full justify-center px-5 py-8 shadow-md bg-gray-800'>
-      <h2 className='text-large text-white text-center' >Created with love by Theo</h2>
-      <h2 className='text-large text-white text-center' >Instagram : <a href="https://www.instagram.com/">theodrsp</a></h2>
+    <div className="flex flex-col w-full justify-center px-5 py-8 shadow-md bg-gray-800">
+      <h2 className="text-large text-white text-center">Created with love by Theo</h2>
+      <h2 className="text-large text-white text-center">
+        Instagram : <a href="https://www.instagram.com/">theodrsp</a>
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
